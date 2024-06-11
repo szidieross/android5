47f91a7e5b1424b23c3901a65987946f84f810ab
szidieross <szidieross@gmail.com>
2024-06-12 00:39:23 +0300

    fixes

--- app/src/main/java/com/example/android5/MainActivity.java
+++ app/src/main/java/com/example/android5/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.android5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.MenuItem;
@@ -9,28 +11,33 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 
 public class MainActivity extends AppCompatActivity {
 
     ListView listView;
-    String[] items = {"Item 1", "Item 2", "Item 3"};
+    List<String> items;
     ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//
-//        Toolbar toolbar = findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        listView = findViewById(R.id.listView);
-//        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
-//        listView.setAdapter(adapter);
-//
-//        registerForContextMenu(listView);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        listView = findViewById(R.id.listView);
+        items = new ArrayList<>(Arrays.asList("Item 1", "Item 2", "Item 3"));
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
+        listView.setAdapter(adapter);
+
+        registerForContextMenu(listView);
     }
 
     @Override
@@ -45,19 +52,13 @@ public class MainActivity extends AppCompatActivity {
         int position = info.position;
         switch (item.getItemId()) {
             case R.id.menu_red:
-                if (listView.getChildAt(position) != null) {
-                    listView.getChildAt(position).setBackgroundColor(getResources().getColor(android.R.color.holo_red_light));
-                }
+                listView.getChildAt(position).setBackgroundColor(Color.RED);
                 return true;
             case R.id.menu_green:
-                if (listView.getChildAt(position) != null) {
-                    listView.getChildAt(position).setBackgroundColor(getResources().getColor(android.R.color.holo_green_light));
-                }
+                listView.getChildAt(position).setBackgroundColor(Color.GREEN);
                 return true;
             case R.id.menu_yellow:
-                if (listView.getChildAt(position) != null) {
-                    listView.getChildAt(position).setBackgroundColor(getResources().getColor(android.R.color.holo_orange_light));
-                }
+                listView.getChildAt(position).setBackgroundColor(Color.YELLOW);
                 return true;
             default:
                 return super.onContextItemSelected(item);
@@ -74,10 +75,14 @@ public class MainActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.menu_sort:
-                // Implement sorting logic here
+                // Sort the list alphabetically
+                Collections.sort(items);
+                adapter.notifyDataSetChanged(); // Notify the adapter of the changes
                 return true;
             case R.id.menu_delete:
-                adapter.clear(); // Clear the adapter to delete all items
+                // Clear the list
+                items.clear();
+                adapter.notifyDataSetChanged(); // Notify the adapter of the changes
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
